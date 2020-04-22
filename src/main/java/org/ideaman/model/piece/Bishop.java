8eeade5b858a5447504bcf4efbb0996a7bfc1af5
Ice <richard.jun.noh@gmail.com>
2020-04-22 15:09:56 -0500

    Fix the movement for Bishop and Rook
    The code that checks their movements were switched. The rook behaved
    like the bishop and vice versa in previous commits.

--- src/main/java/org/ideaman/model/piece/Bishop.java
+++ src/main/java/org/ideaman/model/piece/Bishop.java
@@ -24,7 +24,7 @@ public class Bishop extends Queen
     public List<Position> validMoveList(Piece[][] board)
     {
         LinkedList<Position> validPositionList = new LinkedList<Position>();
-        checkLateral(board, validPositionList);
+        checkDiagonal(board, validPositionList);
         return validPositionList;
     }
 }
--- src/main/java/org/ideaman/model/piece/Rook.java
+++ src/main/java/org/ideaman/model/piece/Rook.java
@@ -24,7 +24,7 @@ public class Rook extends Queen
     public List<Position> validMoveList(Piece[][] board)
     {
         LinkedList<Position> validPositionList = new LinkedList<Position>();
-        checkDiagonal(board, validPositionList);
+        checkLateral(board, validPositionList);
         return validPositionList;
     }
 }
